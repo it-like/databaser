f3883ea6fc78b33d2129180b5c3ad69f301fd1f6
Gustav Gille <gustav.gille@icloud.com>
2023-01-25 11:51:40 +0100

    tables
    first tables

---
+++ createTables.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Branches(
+    name    TEXT
+    program TEXT(5)
+    PRIMARY KEY (name, program)
+)
+
+CREATE TABLE Students(
+    idnr TEXT PRIMARY KEY
+    name TEXT(2)
+    credits TEXT(3)
+    
+
+)
