0114ab39fde0f3078762096a7ed4e73c8f3756bb
Malin Kihlström <malinkih@chalmers.se>
2023-01-25 12:36:33 +0100

    Create table från mandatory branch

--- createTables.sql
+++ createTables.sql
@@ -11,3 +11,49 @@ CREATE TABLE Students(
     
 
 )
+
+
+CREATE TABLE MandatoryBranch(
+    course VARCHAR(6),
+    branch TEXT,
+    program TEXT(5),
+    PRIMARY KEY (course, branch, program),
+    FOREIGN KEY (course) REFERENCES Courses (code),
+    FOREIGN KEY (branch, program) REFERENCES Branches (name, program),
+);
+
+CREATE TABLE RecommendedBranch(
+    course VARCHAR(6),
+    branch TEXT,
+    program TEXT(5),
+    PRIMARY KEY (course, branch, program),
+    FOREIGN KEY (course) REFERENCES Courses (code),
+    FOREIGN KEY (branch, program) REFERENCES Branches (name, program),
+);
+
+CREATE TABLE Registered(
+    student TEXT,
+    course VARCHAR(6),
+    PRIMARY KEY (student, course),
+    FOREIGN KEY (student) REFERENCES Students (idnr),
+    FOREIGN KEY (course) REFERENCES Courses (code),
+);
+
+CREATE TABLE Taken(
+    student TEXT,
+    course VARCHAR(6),
+    grade INT(1) NOT NULL,
+    PRIMARY KEY (student,course),
+    FOREIGN KEY (student) REFERENCES Students (idnr),
+    FOREIGN KEY (course) REFERENCES Courses (code),
+);
+
+CREATE TABLE WaitingList(
+    student TEXT,
+    course VARCHAR(6),
+    position INT (1) NOT NULL,
+    PRIMARY KEY(student,course),
+    FOREIGN KEY (student) REFERENCES Students (idnr),
+    FOREIGN KEY (course) REFERENCES LimitedCourses (code)
+
+);
