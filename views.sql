84a13aeed0ca1dbabe4c2b754d73f2629a995ede
Gustav Gille <gustav.gille@icloud.com>
2023-02-02 13:56:46 +0100

    one step clåser
    La till lite ändringar, får error att första kolumnen inte hittas, tror att det har något med nycklar att göra

--- views.sql
+++ views.sql
@@ -1,17 +1,16 @@
 --SELECT idnr, name, login, program, branch FROM BasicInformation ORDER BY idnr;
-
 CREATE VIEW BasicInformation AS
-    SELECT Students.name, idnr, login, Students.program, branch
+    SELECT idnr, Students.name, login, Students.program, branch
     FROM Students, StudentBranches
     WHERE student = idnr;
 
-    
---SELECT student, course, grade, credits FROM FinishedCourses ORDER BY student;
 
+--SELECT student, course, grade, credits FROM FinishedCourses ORDER BY student;
 CREATE VIEW FinishedCourses AS
     SELECT student, course, grade, Courses.credits
     FROM Taken, Students, Courses
     Where student = Students.idnr AND course = Courses.code AND credits = Courses.credits;
+
 
 --SELECT student, course, credits FROM PassedCourses ORDER BY student;
 CREATE VIEW PassedCourses AS
@@ -26,21 +25,30 @@ CREATE VIEW Registrations AS
         UNION
     SELECT student, course, 'registered' AS status FROM Registered;
     
+
 --SELECT student, course FROM UnreadMandatory ORDER BY student;
 CREATE VIEW UnreadMandatory AS
     SELECT student, course
     FROM Taken, MandatoryProgram
     WHERE code != course;
 
+
 --SELECT student, totalCredits, mandatoryLeft, mathCredits, researchCredits, seminarCourses, qualified FROM PathToGraduation ORDER BY student;
-CREATE TABLE PathToGraduation AS
-SELECT idnr, --student
-SUM(credits), --totalcredits
-UnreadMandatory.course,
-SUM(credits) INTERSECT SELECT Classified.classification = 'math', --mathcredits
-SUM(credits) INTERSECT SELECT Classified.classification = 'research',--research credits
-COUNT(PassedCourses.course) INTERSECT SELECT Classified.classification = 'research'
-couse FROM PassedCourses IN code FROM MandatoryProgram
-AND SUM(PassedCourses.credits) >=10 AND mathCredits >= 20 AND researchCredits >= 10 AND seminarCourses
-FROM BasicInformation, Courses, Taken
-WHERE taken.course = code AND student.idnr = taken.student AND Classified.course = code
+CREATE TABLE PathToGraduation AS 
+    SELECT student, --student
+    SUM(PassedCourses.credits), --totalcredits
+    COUNT(UnreadMandatory.course),
+    SUM(credits) INTERSECT SELECT Classified.classification = 'math', --mathCredits
+    SUM(credits) INTERSECT SELECT Classified.classification = 'research',--researchCredits
+    COUNT(PassedCourses.course) INTERSECT SELECT Classified.classification = 'research', 
+    CASE WHEN
+        mandatoryLeft IS NULL AND UnreadMandatory IS NOT NULL 
+        AND seminarCourses > 0 
+        AND SUM(PassedCourses.credits) >=10 
+        AND mathCredits >= 20 
+        AND researchCredits >= 10 
+        AND seminarCourses
+    IS NULL THEN "False" ELSE "True"
+    END
+    FROM BasicInformation, Courses, Taken
+    WHERE student = BasicInformation.name AND taken.course = code AND student.idnr = taken.student AND Classified.course = code;
