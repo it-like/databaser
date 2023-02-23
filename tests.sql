3904d096f39294d0d4d15d495206e2cda44969bb
Malin Kihlström <malinkih@chalmers.se>
2023-02-23 12:52:30 +0100

    (づ￣ 3￣)づ

--- tests.sql
+++ tests.sql
@@ -5,35 +5,45 @@ INSERT INTO Registrations VALUES ('7777777777', 'CCC444');
 
 -- TEST #2: Register an already registered student.
 -- EXPECTED OUTCOME: Fail
-INSERT INTO Registered VALUES ('1111111111','CCC111');
+--INSERT INTO Registered VALUES ('1111111111','CCC111');
 
 
 -- TEST #3: registered to a limited course
 -- EXPECTED OUTCOME: Pass
-INSERT INTO Registered VALUES('7777777777', 'CCC222');
+INSERT INTO Registrations VALUES('9999999999', 'CCC666');
+
 
 -- TEST #4: waiting for a limited course
 -- EXPECTED OUTCOME: Pass
+INSERT INTO Registrations VALUES('8888888888', 'CCC222');
 
-INSERT INTO WaitingList VALUES('8888888888', 'CCC222', 3)
 
 -- TEST #5: removed from a waiting list (with additional students in it
 -- EXPECTED OUTCOME: Pass
+DELETE FROM Registrations WHERE student = '3333333333' AND course = 'CCC333';
 
-DELETE FROM WaitingList WHERE student = '8888888888';
 
 -- TEST #6: unregistered from a limited course without a waiting list;
 -- EXPECTED OUTCOME: Pass
+DELETE FROM Registrations WHERE student = '9999999999' AND course = 'CCC666';
 
 
 -- TEST #7: unregistered from a limited course with a waiting list, when the student is registered;
 -- EXPECTED OUTCOME: Pass
 
---
+DELETE FROM Registrations WHERE student = '2222222222' AND course = 'CCC222';
+
 
 -- TEST #8: unregistered from a limited course with a waiting list, when the student is in the middle of the waiting list;
--- EXPECTED OUTCOME: 
---
+-- EXPECTED OUTCOME: Pass
+
+DELETE FROM Registrations WHERE student = '3333333333' AND course = 'CCC222';
+
 
 -- TEST #9: unregistered from an overfull course with a waiting list.
--- EXPECTED OUTCOME: 
+-- EXPECTED OUTCOME: Pass
+
+DELETE FROM Registrations WHERE Student = '9999999999' AND course = 'CCC222';
+
+SELECT * FROM Registrations;
+SELECT * FROM CourseQueuePositions;
