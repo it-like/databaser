2a03cd64dadffc277b0c98cd2cde2d61395aa392
Gustav Gille <gustav.gille@icloud.com>
2023-03-02 12:25:49 +0100

    reeee

--- tests.sql
+++ tests.sql
@@ -3,8 +3,6 @@
 --INSERT INTO Registrations VALUES ('7777777777', 'CCC333'); 
 --INSERT INTO Registrations VALUES ('7777777777', 'CCC444'); 
 
-INSERT INTO Registrations VALUES ('7777777777', 'CCC777'); 
-/*
 -- TEST #2: Register an already registered student.
 -- EXPECTED OUTCOME: Fail
 INSERT INTO Registered VALUES ('1111111111','CCC111');
@@ -19,7 +17,6 @@ INSERT INTO Registrations VALUES('8888888888', 'CCC222');
 
 -- TEST #5: removed from a waiting list (with additional students in it
 -- EXPECTED OUTCOME: Pass
---DELETE FROM Registrations WHERE student = '8888888888' AND course = 'CCC222';
 DELETE FROM Registrations WHERE student = '8888888888' AND course = 'CCC222';
 
 -- TEST #6: unregistered from a limited course with a waiting list, when the student is registered;
@@ -40,9 +37,8 @@ DELETE FROM Registrations WHERE Student = '1111111111' AND course = 'CCC222';
 
 --TEST #10: resgiter to an unlimited course with met prerequisites 
 --EXPECTED OUTCOME : Pass
-INSERT INTO Registrations WHERE Student = '6666666666' AND course = 'CCC555'
+INSERT INTO Registrations VALUES('6666666666','CCC555');
 
 --TEST #11: register to a course with unmet prerequisites
 --EXPECTED OUTCOME : fail
-INSERT INTO Registrations WHERE Student = '2222222222' AND course = 'CCC555'
-*/
+INSERT INTO Registrations VALUES('2222222222','CCC555');
